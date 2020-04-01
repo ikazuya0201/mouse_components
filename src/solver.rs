1104968a6d51d628241f33c7cc5b0cabaa4f1f32
Kazuya Iimuro <ikazuya0201@gmail.com>
2020-04-01 17:02:41 +0900

    [update] Modified to use result.

--- src/solver.rs
+++ src/solver.rs
@@ -22,12 +22,12 @@ where
 impl<Node, GL> Solver<Node, GL>
 where
     GL: ArrayLength<Node>,
-    Node: Clone + Copy,
+    Node: Clone + Copy + core::fmt::Debug,
 {
     pub fn new(start: Node, goals: &[Node]) -> Self {
         let mut goal_vec = Vec::new();
         for &goal in goals {
-            goal_vec.push(goal);
+            goal_vec.push(goal).unwrap();
         }
         Self {
             start: start,
@@ -43,8 +43,8 @@ where
     ) -> Vec<Node, L>
     where
         Graph: operator::Graph<Node, Cost, Direction>,
-        Cost: PartialOrd + Ord + Clone + Copy + Add<Output = Cost> + Bounded,
-        Node: PartialOrd + Ord + Default + Clone + Copy + Into<usize>,
+        Cost: PartialOrd + Ord + Clone + Copy + Add<Output = Cost> + Bounded + core::fmt::Debug,
+        Node: PartialOrd + Ord + Default + Clone + Copy + Into<usize> + core::fmt::Debug,
         L: ArrayLength<Node>
             + ArrayLength<(Cost, Node)>
             + ArrayLength<(Node, Cost)>
@@ -61,21 +61,21 @@ where
             .take(L::to_usize())
             .collect::<GenericArray<Node, L>>();
 
-        heap.push((Cost::min_value(), start));
+        heap.push((Cost::min_value(), start)).unwrap();
         dist[start.into()] = Cost::min_value();
 
         let construct_path = |goal: Node, prev: GenericArray<Node, L>| {
             let mut rpath = Vec::<Node, L>::new();
-            rpath.push(goal);
+            rpath.push(goal).unwrap();
             let mut current = goal;
             while current != self.start {
                 let next = prev[current.into()];
-                rpath.push(next);
+                rpath.push(next).unwrap();
                 current = next;
             }
             let mut path = Vec::new();
             for node in rpath {
-                path.push(node);
+                path.push(node).unwrap();
             }
             path
         };
@@ -92,7 +92,7 @@ where
                     let ncost = cost + c;
                     dist[next.into()] = ncost;
                     prev[next.into()] = node;
-                    heap.push((ncost, next));
+                    heap.push((ncost, next)).unwrap();
                 }
             }
         }
@@ -105,8 +105,8 @@ impl<Node, Cost, Graph, L, GL> operator::Solver<Node, Cost, Direction, Graph, L>
     for Solver<Node, GL>
 where
     Graph: operator::Graph<Node, Cost, Direction>,
-    Cost: PartialOrd + Ord + Clone + Copy + Add<Output = Cost> + Bounded,
-    Node: PartialOrd + Ord + Default + Clone + Copy + Into<usize>,
+    Cost: PartialOrd + Ord + Clone + Copy + Add<Output = Cost> + Bounded + core::fmt::Debug,
+    Node: PartialOrd + Ord + Default + Clone + Copy + Into<usize> + core::fmt::Debug,
     L: ArrayLength<Node>
         + ArrayLength<(Cost, Node)>
         + ArrayLength<(Node, Cost)>
