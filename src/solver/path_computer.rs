4afecdc416aa2782a0495104d61918055395b958
Kazuya Iimuro <ikazuya0201@gmail.com>
2020-04-10 04:47:43 +0900

    [fix] Fixed path computer and added tests.

--- src/solver/path_computer.rs
+++ src/solver/path_computer.rs
@@ -54,8 +54,8 @@ where
 
     fn initialize(&mut self) {
         for i in 0..L::to_usize() {
-            self.g[i] = Cost::min_value();
-            self.rhs[i] = Cost::min_value();
+            self.g[i] = Cost::max_value();
+            self.rhs[i] = Cost::max_value();
         }
         self.rhs[self.start.into()] = Cost::min_value();
         self.heap
@@ -94,11 +94,14 @@ where
         Graph: operator::Graph<Node, Cost, Direction>,
     {
         let mut is_updated = false;
-        while (self.heap.peek().unwrap().1).0 < self.calculate_value(self.goal)
-            || self.rhs[self.goal.into()] != self.g[self.goal.into()]
-        {
+        while let Some((node, Reverse(value))) = self.heap.pop() {
+            if self.calculate_value(self.goal) <= value
+                && self.rhs[self.goal.into()] == self.g[self.goal.into()]
+            {
+                self.heap.push(node, Reverse(value)).unwrap();
+                break;
+            }
             is_updated = true;
-            let (node, _) = self.heap.pop().unwrap();
             if self.g[node.into()] > self.rhs[node.into()] {
                 self.g[node.into()] = self.rhs[node.into()];
             } else {
@@ -146,5 +149,100 @@ where
 
     pub fn get_shortest_path(&self) -> &Vec<Node, L> {
         &self.shortest_path
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use heapless::consts::*;
+
+    struct Graph {
+        mat: GenericArray<GenericArray<Option<usize>, U8>, U8>,
+    }
+
+    impl Graph {
+        fn new(edges: &[(usize, usize, usize)]) -> Self {
+            let mut mat = GenericArray::<GenericArray<Option<usize>, U8>, U8>::default();
+            for &(src, dst, cost) in edges {
+                mat[src][dst] = Some(cost);
+            }
+            Self { mat: mat }
+        }
+    }
+
+    impl operator::Graph<usize, usize, bool> for Graph {
+        fn successors<L: ArrayLength<(usize, usize)>>(
+            &self,
+            node: usize,
+        ) -> Vec<(usize, usize), L> {
+            let mut succ = Vec::new();
+            for i in 0..L::to_usize() {
+                if let Some(cost) = self.mat[node][i] {
+                    succ.push((i, cost)).unwrap();
+                }
+            }
+            succ
+        }
+
+        fn predecessors<L: ArrayLength<(usize, usize)>>(
+            &self,
+            node: usize,
+        ) -> Vec<(usize, usize), L> {
+            let mut pred = Vec::new();
+            for i in 0..L::to_usize() {
+                if let Some(cost) = self.mat[i][node] {
+                    pred.push((i, cost)).unwrap();
+                }
+            }
+            pred
+        }
+
+        fn is_checked(&self, _edge: (usize, usize)) -> bool {
+            true
+        }
+
+        fn edge_direction(&self, _edge: (usize, usize)) -> bool {
+            true
+        }
+        fn separate_to_unit_edges<L: ArrayLength<(usize, usize)>>(
+            &self,
+            _edge: (usize, usize),
+        ) -> Vec<(usize, usize), L> {
+            Vec::new()
+        }
+    }
+
+    impl Graph {
+        fn update_edge(&mut self, src: usize, dst: usize, cost: usize) {
+            self.mat[src][dst] = Some(cost);
+        }
+    }
+
+    #[test]
+    fn shortest_path() {
+        let start = 0usize;
+        let goal = 6usize;
+        let directed_edges = [
+            (0, 1, 10),
+            (0, 2, 8),
+            (1, 3, 5),
+            (2, 3, 2),
+            (3, 4, 9),
+            (3, 5, 7),
+            (4, 6, 2),
+            (5, 6, 3),
+        ];
+
+        let mut graph = Graph::new(&directed_edges);
+        let mut computer = PathComputer::<usize, usize, U8>::new(start, goal);
+
+        computer.update_shortest_path(&graph);
+        assert_eq!(computer.get_shortest_path().as_ref(), [0, 2, 3, 5, 6]);
+
+        graph.update_edge(3, 4, 1);
+        computer.update_node(4, &graph);
+        computer.update_shortest_path(&graph);
+        assert_eq!(computer.get_shortest_path().as_ref(), [0, 2, 3, 4, 6]);
     }
 }
