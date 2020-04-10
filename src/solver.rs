f4d2f3ca24e24ecab6a85e0cd92d9ea4d11a242d
Kazuya Iimuro <ikazuya0201@gmail.com>
2020-04-10 19:49:36 +0900

    [update] Added a method to solver.

--- src/solver.rs
+++ src/solver.rs
@@ -3,6 +3,7 @@ mod path_computer;
 use core::cmp::Reverse;
 use core::fmt::Debug;
 
+use generic_array::GenericArray;
 use heapless::{ArrayLength, Vec};
 use num::{Bounded, Saturating};
 
@@ -31,6 +32,7 @@ where
         + ArrayLength<Option<usize>>
         + ArrayLength<(Node, Cost)>
         + ArrayLength<(Node, Reverse<Cost>)>
+        + ArrayLength<bool>
         + ArrayLength<Node>,
 {
     pub fn new<Graph>(start: Node, goal: Node, graph: &Graph) -> Self
@@ -42,6 +44,21 @@ where
             goal: goal,
             path_computer: PathComputer::new(start, goal, graph),
         }
+    }
+
+    pub fn get_target_nodes<Graph>(&self, path: &[Node], graph: &Graph) -> GenericArray<bool, L>
+    where
+        Graph: operator::CheckableGraph<Node, Cost>,
+    {
+        let mut is_target_node = GenericArray::default();
+        for i in 0..path.len() - 1 {
+            if !graph.is_checked((path[i], path[i + 1])) {
+                for node in graph.unchecked_edge_to_target_nodes((path[i], path[i + 1])) {
+                    is_target_node[node.into()] = true;
+                }
+            }
+        }
+        is_target_node
     }
 }
 
@@ -55,6 +72,7 @@ where
         + ArrayLength<Option<usize>>
         + ArrayLength<(Node, Cost)>
         + ArrayLength<(Node, Reverse<Cost>)>
+        + ArrayLength<bool>
         + ArrayLength<Node>,
 {
     fn start(&self) -> Node {
@@ -67,6 +85,8 @@ where
         graph: &Graph,
     ) -> Option<(Vec<Node, L>, fn(fn(Direction) -> bool) -> Direction)> {
         let shortest_path = self.path_computer.get_shortest_path(graph);
+
+        let is_target_node = self.get_target_nodes(&shortest_path, graph);
 
         let dummy = |f: fn(Direction) -> bool| Direction::North;
         Some((Vec::new(), dummy))
