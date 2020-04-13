e818069440104964bbc9a7baea2da7f5831b073f
Kazuya Iimuro <ikazuya0201@gmail.com>
2020-04-13 13:33:27 +0900

    [update] Modified methods of solver to functions.

--- src/solver.rs
+++ src/solver.rs
@@ -53,148 +53,6 @@ where
         }
     }
 
-    fn get_checker_nodes<Graph>(&self, path: &[Node], graph: &Graph) -> GenericArray<bool, L>
-    where
-        Graph: operator::CheckableGraph<Node, Cost>,
-    {
-        let mut is_checker_node = GenericArray::default();
-        for i in 0..path.len() - 1 {
-            if !graph.is_checked((path[i], path[i + 1])) {
-                for node in graph.unchecked_edge_to_checker_nodes((path[i], path[i + 1])) {
-                    is_checker_node[node.into()] = true;
-                }
-            }
-        }
-        is_checker_node
-    }
-
-    fn compute_shortest_path<Graph>(
-        &self,
-        start: Node,
-        is_goal: &[bool],
-        graph: &Graph,
-    ) -> Option<Vec<Node, L>>
-    where
-        Graph: operator::Graph<Node, Cost>,
-        L: ArrayLength<Cost> + ArrayLength<Option<Node>>,
-    {
-        let mut heap = BinaryHeap::<Node, Reverse<Cost>, L>::new();
-        let mut dist = GenericArray::<Cost, L>::default();
-        let mut prev = GenericArray::<Option<Node>, L>::default();
-        for i in 0..L::to_usize() {
-            dist[i] = Cost::max_value();
-        }
-
-        heap.push(start, Reverse(Cost::min_value())).unwrap();
-        dist[start.into()] = Cost::min_value();
-
-        let construct_path = |goal: Node, prev: GenericArray<Option<Node>, L>| {
-            let mut rpath = Vec::<Node, L>::new();
-            let mut current = goal;
-            rpath.push(goal).unwrap();
-            while let Some(next) = prev[current.into()] {
-                rpath.push(next).unwrap();
-                current = next;
-                if next == start {
-                    break;
-                }
-            }
-            let mut path = Vec::new();
-            for i in 0..rpath.len() {
-                path.push(rpath[rpath.len() - i - 1]).unwrap();
-            }
-            path
-        };
-
-        while let Some((node, Reverse(cost))) = heap.pop() {
-            if is_goal[node.into()] {
-                return Some(construct_path(node, prev));
-            }
-            for (succ, scost) in graph.successors(node) {
-                let ncost = cost.saturating_add(scost);
-                if dist[succ.into()] > ncost {
-                    dist[succ.into()] = ncost;
-                    prev[succ.into()] = Some(node);
-                    heap.push_or_update(succ, Reverse(ncost)).unwrap();
-                }
-            }
-        }
-        None
-    }
-
-    fn compute_checked_shortest_path<Graph>(
-        &self,
-        start: Node,
-        goal: Node,
-        graph: &Graph,
-    ) -> Vec<Node, L>
-    where
-        Graph: operator::CheckableGraph<Node, Cost>,
-        L: ArrayLength<Option<Node>>,
-    {
-        let mut heap = BinaryHeap::<Node, Reverse<Cost>, L>::new();
-        let mut dist = GenericArray::<Cost, L>::default();
-        let mut prev = GenericArray::<Option<Node>, L>::default();
-        for i in 0..L::to_usize() {
-            dist[i] = Cost::max_value();
-        }
-
-        heap.push(goal, Reverse(Cost::min_value())).unwrap();
-        dist[goal.into()] = Cost::min_value();
-
-        let construct_path = |goal: Node, prev: GenericArray<Option<Node>, L>| {
-            let mut path = Vec::<Node, L>::new();
-            let mut current = start;
-            path.push(goal).unwrap();
-            while let Some(next) = prev[current.into()] {
-                path.push(next).unwrap();
-                current = next;
-                if next == goal {
-                    break;
-                }
-            }
-            path
-        };
-
-        while let Some((node, Reverse(cost))) = heap.pop() {
-            if node == start {
-                return construct_path(node, prev);
-            }
-            for (pred, pcost) in graph.checked_predecessors(node) {
-                let ncost = cost.saturating_add(pcost);
-                if dist[pred.into()] > ncost {
-                    dist[pred.into()] = ncost;
-                    prev[pred.into()] = Some(node);
-                    heap.push_or_update(pred, Reverse(ncost)).unwrap();
-                }
-            }
-        }
-        unreachable!();
-    }
-
-    fn find_first_checker_node_and_next_direction<Graph>(
-        &self,
-        path: &[Node],
-        graph: &Graph,
-    ) -> Option<(Node, Direction)>
-    where
-        Graph: operator::DirectionalGraph<Node, Cost, Direction>,
-    {
-        for i in 0..path.len() - 1 {
-            if !graph.is_checked((path[i], path[i + 1])) {
-                return Some(
-                    graph.find_first_checker_node_and_next_direction((path[i], path[i + 1])),
-                );
-            }
-        }
-        let last_node = path[path.len() - 1];
-        let nearest_unchecked_node = graph.nearest_unchecked_node(last_node)?;
-        Some((
-            last_node,
-            graph.edge_direction((last_node, nearest_unchecked_node)),
-        ))
-    }
-
     fn compute_direction_sequence<Graph>(
         &self,
         start_node: Node,
@@ -218,9 +76,9 @@ where
             }
             path_computer.compute_shortest_path(&graph);
             let shortest_path = path_computer.get_shortest_path(&graph);
-            let is_checker = self.get_checker_nodes(&shortest_path, &graph);
+            let is_checker: GenericArray<bool, L> = get_checker_nodes(&shortest_path, &graph);
             if let Some(path_to_checker) =
-                self.compute_shortest_path(start_node, &is_checker, &graph)
+                compute_shortest_path::<Node, Cost, Graph, L>(start_node, &is_checker, &graph)
             {
                 if path_to_checker.is_empty() {
                     break;
@@ -259,17 +117,169 @@ where
     fn solve(&self, current: Node, graph: &Graph) -> Option<(Vec<Node, L>, Self::Directions)> {
         let shortest_path = self.path_computer.get_shortest_path(graph);
 
-        let is_checker = self.get_checker_nodes(&shortest_path, graph);
+        let is_checker: GenericArray<bool, L> = get_checker_nodes(&shortest_path, graph);
 
-        let path_to_checker = self.compute_shortest_path(current, &is_checker, graph)?;
+        let path_to_checker: Vec<Node, L> = compute_shortest_path(current, &is_checker, graph)?;
 
         let (checker, direction) =
-            self.find_first_checker_node_and_next_direction(&path_to_checker, graph)?;
+            find_first_checker_node_and_next_direction(&path_to_checker, graph)?;
 
-        let path = self.compute_checked_shortest_path(current, checker, graph);
+        let path = compute_checked_shortest_path(current, checker, graph)?;
 
         let directions = self.compute_direction_sequence(checker, direction, graph);
 
         Some((path, directions))
     }
+}
+
+fn get_checker_nodes<Node, Cost, Graph, L>(path: &[Node], graph: &Graph) -> GenericArray<bool, L>
+where
+    Node: Into<usize> + Clone + Copy,
+    Graph: operator::CheckableGraph<Node, Cost>,
+    L: ArrayLength<bool>,
+{
+    let mut is_checker_node = GenericArray::default();
+    for i in 0..path.len() - 1 {
+        if !graph.is_checked((path[i], path[i + 1])) {
+            for node in graph.unchecked_edge_to_checker_nodes((path[i], path[i + 1])) {
+                is_checker_node[node.into()] = true;
+            }
+        }
+    }
+    is_checker_node
+}
+
+fn compute_shortest_path<Node, Cost, Graph, L>(
+    start: Node,
+    is_goal: &[bool],
+    graph: &Graph,
+) -> Option<Vec<Node, L>>
+where
+    Node: Into<usize> + Clone + Copy + Debug + Eq,
+    Cost: Clone + Copy + Ord + Default + Bounded + Debug + Saturating,
+    Graph: operator::Graph<Node, Cost>,
+    L: ArrayLength<Cost>
+        + ArrayLength<Option<Node>>
+        + ArrayLength<(Node, Reverse<Cost>)>
+        + ArrayLength<Option<usize>>
+        + ArrayLength<Node>,
+{
+    let mut heap = BinaryHeap::<Node, Reverse<Cost>, L>::new();
+    let mut dist = GenericArray::<Cost, L>::default();
+    let mut prev = GenericArray::<Option<Node>, L>::default();
+    for i in 0..L::to_usize() {
+        dist[i] = Cost::max_value();
+    }
+
+    heap.push(start, Reverse(Cost::min_value())).unwrap();
+    dist[start.into()] = Cost::min_value();
+
+    let construct_path = |goal: Node, prev: GenericArray<Option<Node>, L>| {
+        let mut rpath = Vec::<Node, L>::new();
+        let mut current = goal;
+        rpath.push(goal).unwrap();
+        while let Some(next) = prev[current.into()] {
+            rpath.push(next).unwrap();
+            current = next;
+            if next == start {
+                break;
+            }
+        }
+        let mut path = Vec::new();
+        for i in 0..rpath.len() {
+            path.push(rpath[rpath.len() - i - 1]).unwrap();
+        }
+        path
+    };
+
+    while let Some((node, Reverse(cost))) = heap.pop() {
+        if is_goal[node.into()] {
+            return Some(construct_path(node, prev));
+        }
+        for (succ, scost) in graph.successors(node) {
+            let ncost = cost.saturating_add(scost);
+            if dist[succ.into()] > ncost {
+                dist[succ.into()] = ncost;
+                prev[succ.into()] = Some(node);
+                heap.push_or_update(succ, Reverse(ncost)).unwrap();
+            }
+        }
+    }
+    None
+}
+
+fn compute_checked_shortest_path<Node, Cost, Graph, L>(
+    start: Node,
+    goal: Node,
+    graph: &Graph,
+) -> Option<Vec<Node, L>>
+where
+    Node: Into<usize> + Clone + Copy + Debug + Eq,
+    Cost: Clone + Copy + Ord + Default + Bounded + Debug + Saturating,
+    Graph: operator::CheckableGraph<Node, Cost>,
+    L: ArrayLength<Cost>
+        + ArrayLength<Option<Node>>
+        + ArrayLength<(Node, Reverse<Cost>)>
+        + ArrayLength<Option<usize>>
+        + ArrayLength<Node>,
+{
+    let mut heap = BinaryHeap::<Node, Reverse<Cost>, L>::new();
+    let mut dist = GenericArray::<Cost, L>::default();
+    let mut prev = GenericArray::<Option<Node>, L>::default();
+    for i in 0..L::to_usize() {
+        dist[i] = Cost::max_value();
+    }
+
+    heap.push(goal, Reverse(Cost::min_value())).unwrap();
+    dist[goal.into()] = Cost::min_value();
+
+    let construct_path = |goal: Node, prev: GenericArray<Option<Node>, L>| {
+        let mut path = Vec::<Node, L>::new();
+        let mut current = start;
+        path.push(goal).unwrap();
+        while let Some(next) = prev[current.into()] {
+            path.push(next).unwrap();
+            current = next;
+            if next == goal {
+                break;
+            }
+        }
+        path
+    };
+
+    while let Some((node, Reverse(cost))) = heap.pop() {
+        if node == start {
+            return Some(construct_path(node, prev));
+        }
+        for (pred, pcost) in graph.checked_predecessors(node) {
+            let ncost = cost.saturating_add(pcost);
+            if dist[pred.into()] > ncost {
+                dist[pred.into()] = ncost;
+                prev[pred.into()] = Some(node);
+                heap.push_or_update(pred, Reverse(ncost)).unwrap();
+            }
+        }
+    }
+    None
+}
+
+fn find_first_checker_node_and_next_direction<Node, Cost, Direction, Graph>(
+    path: &[Node],
+    graph: &Graph,
+) -> Option<(Node, Direction)>
+where
+    Node: Clone + Copy,
+    Graph: operator::DirectionalGraph<Node, Cost, Direction>,
+{
+    for i in 0..path.len() - 1 {
+        if !graph.is_checked((path[i], path[i + 1])) {
+            return Some(graph.find_first_checker_node_and_next_direction((path[i], path[i + 1])));
+        }
+    }
+    let last_node = path[path.len() - 1];
+    let nearest_unchecked_node = graph.nearest_unchecked_node(last_node)?;
+    Some((
+        last_node,
+        graph.edge_direction((last_node, nearest_unchecked_node)),
+    ))
 }
