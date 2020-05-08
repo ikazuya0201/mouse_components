a0f5f9d35ff1b35223084879467971ae98e7c164
Kazuya Iimuro <ikazuya0201@gmail.com>
2020-05-09 02:00:10 +0900

    [update] Modified the interfaces between maze and agent.

--- src/operator/agent.rs
+++ src/operator/agent.rs
@@ -1,8 +1,8 @@
-pub trait Agent<Position, Pattern> {
+pub trait Agent<Position, Direction> {
     type Positions: IntoIterator<Item = Position>;
 
     fn existing_obstacles(&self) -> Self::Positions;
-    fn set_instructed_pattern(&self, pattern: Pattern);
+    fn set_instructed_direction(&self, direction: Direction);
     //This method is called by interrupt.
     fn track_next(&self);
 }
