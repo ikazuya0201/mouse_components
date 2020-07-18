8e789cb6c04703f9df68e9311254271c21267f03
Kazuya Iimuro <ikazuya0201@gmail.com>
2020-07-19 05:40:29 +0900

    [add] Implemented agent framework and defined some traits.

--- src/administrator/agent.rs
+++ src/administrator/agent.rs
@@ -1,8 +1,8 @@
-pub trait Agent<Position, Direction> {
-    type Positions: IntoIterator<Item = Position>;
+pub trait Agent<Obstacle, Pose, Direction> {
+    type Obstacles: IntoIterator<Item = Obstacle>;
 
-    fn get_existing_obstacles(&self) -> Self::Positions;
-    fn set_instructed_direction(&self, direction: Direction);
+    fn get_existing_obstacles(&self) -> Self::Obstacles;
+    fn set_instructed_direction(&self, pose: Pose, direction: Direction);
     //This method is called by interrupt.
     fn track_next(&self);
 }
--- src/administrator/maze.rs
+++ src/administrator/maze.rs
@@ -3,8 +3,8 @@ pub trait Storable {
     fn restore(&self);
 }
 
-pub trait ObstacleInterpreter<Position> {
-    fn interpret_obstacles<Positions: IntoIterator<Item = Position>>(&self, positions: Positions);
+pub trait ObstacleInterpreter<Obstacle> {
+    fn interpret_obstacles<Obstacles: IntoIterator<Item = Obstacle>>(&self, obstacles: Obstacles);
 }
 
 //The trait method could be called by other threads.
@@ -16,6 +16,8 @@ pub trait DirectionInstructor<Node, Direction> {
 pub trait GraphConverter<Node, SearchNode> {
     type SearchNodes: IntoIterator<Item = SearchNode>;
 
+    ///checker nodes are search nodes on which agent can check
+    ///if walls on the given path exist.
     fn convert_to_checker_nodes<Nodes: IntoIterator<Item = Node>>(
         &self,
         path: Nodes,
@@ -27,4 +29,9 @@ pub trait Graph<Node, Cost> {
 
     fn successors(&self, node: Node) -> Self::Edges;
     fn predecessors(&self, node: Node) -> Self::Edges;
+}
+
+///convert node to pose
+pub trait NodeConverter<Node, Pose> {
+    fn convert(&self, node: Node) -> Pose;
 }
