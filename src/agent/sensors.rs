4e1965c2f82ffc1513a08bccdb76baf8c870e7a3
Kazuya Iimuro <ikazuya0201@gmail.com>
2020-07-21 12:15:38 +0900

    [update] Moved and updated sensors.

--- src/agent/sensors.rs
+++ src/agent/sensors.rs
@@ -1,3 +1,1 @@
 pub mod distance_sensor;
-pub mod encoder;
-pub mod imu;
---
+++ src/estimator/imu.rs
@@ -0,0 +1,32 @@
+use crate::utils::vector::Vector3;
+
+use quantities::{Acceleration, AngularSpeed};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct IMUError;
+
+pub trait IMU {
+    fn get_angular_speed_x(&mut self) -> nb::Result<AngularSpeed, IMUError>;
+    fn get_angular_speed_y(&mut self) -> nb::Result<AngularSpeed, IMUError>;
+    fn get_angular_speed_z(&mut self) -> nb::Result<AngularSpeed, IMUError>;
+
+    fn get_acceleration_x(&mut self) -> nb::Result<Acceleration, IMUError>;
+    fn get_acceleration_y(&mut self) -> nb::Result<Acceleration, IMUError>;
+    fn get_acceleration_z(&mut self) -> nb::Result<Acceleration, IMUError>;
+
+    fn get_angular_speeds(&mut self) -> nb::Result<Vector3<AngularSpeed>, IMUError> {
+        Ok(Vector3 {
+            x: self.get_angular_speed_x()?,
+            y: self.get_angular_speed_y()?,
+            z: self.get_angular_speed_z()?,
+        })
+    }
+
+    fn get_accelerations(&mut self) -> nb::Result<Vector3<Acceleration>, IMUError> {
+        Ok(Vector3 {
+            x: self.get_acceleration_x()?,
+            y: self.get_acceleration_y()?,
+            z: self.get_acceleration_z()?,
+        })
+    }
+}
