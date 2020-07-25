dd7ee969c53d17ee92142392bfb6b20873a96d31
Kazuya Iimuro <ikazuya0201@gmail.com>
2020-07-25 23:54:48 +0900

    [update] Implemented Debug and Clone for State.

--- src/tracker/state.rs
+++ src/tracker/state.rs
@@ -1,21 +1,24 @@
 use core::ops::Div;
 
-use quantities::{Angle, Distance, Time, TimeDifferentiable};
+use quantities::{Angle, Distance, Quantity, Time, TimeDifferentiable};
 
 use crate::{ddt, dt};
 
+#[derive(Clone, Debug)]
 pub struct State {
     pub x: SubState<Distance>,
     pub y: SubState<Distance>,
     pub theta: SubState<Angle>,
 }
 
+#[derive(Clone, Debug)]
 pub struct SubState<T>
 where
-    T: TimeDifferentiable,
-    dt!(T): TimeDifferentiable,
+    T: TimeDifferentiable + core::fmt::Debug,
+    dt!(T): TimeDifferentiable + core::fmt::Debug,
+    ddt!(T): Quantity + core::fmt::Debug,
 {
     pub x: T,
-    pub v: dt!(T),
-    pub a: ddt!(T),
+    pub v: <T as Div<Time>>::Output,
+    pub a: <<T as Div<Time>>::Output as Div<Time>>::Output,
 }
