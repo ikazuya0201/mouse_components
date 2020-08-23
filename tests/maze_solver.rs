b902300b32899a1890b282a714d6157dd7dd444d
Kazuya Iimuro <ikazuya0201@gmail.com>
2020-08-24 01:05:53 +0900

    [update] Added maze solver test.

--- tests/maze_solver.rs
+++ tests/maze_solver.rs
@@ -77,39 +77,71 @@ fn test_compute_shortest_path_u4() {
     }
 }
 
-#[test]
-fn test_next_node_candidates_u4() {
-    use generic_array::arr;
-    use AbsoluteDirection::*;
+macro_rules! next_node_candidates_tests {
+    ($($name: ident: $value: expr,)*) => {
+        $(
+                #[test]
+                fn $name() {
+                    use generic_array::arr;
+                    use AbsoluteDirection::*;
+                    use WallDirection::*;
 
-    let new = |x, y, dir| NodeId::<U4>::new(x, y, dir).unwrap();
-    let new_search = |x, y, dir| SearchNodeId::<U4>::new(x, y, dir).unwrap();
-    let new_wall = |x, y, z| WallPosition::<U4>::new(x, y, z).unwrap();
+                    let new = |x, y, dir| NodeId::<U4>::new(x, y, dir).unwrap();
+                    let new_search = |x, y, dir| SearchNodeId::<U4>::new(x, y, dir).unwrap();
+                    let new_wall = |x, y, z| WallPosition::<U4>::new(x, y, z).unwrap();
 
-    let start = new(0, 0, North);
-    let goals = arr![NodeId<U4>; new(2,0,West), new(2,0,South)];
+                    let start = new(0, 0, North);
+                    let goals = arr![NodeId<U4>; new(2,0,West), new(2,0,South)];
 
-    let solver = Solver::<NodeId<U4>, SearchNodeId<U4>, U256, _>::new(start, goals);
+                    let solver = Solver::<NodeId<U4>, SearchNodeId<U4>, U256, _>::new(start, goals);
 
-    let right = WallDirection::Right;
-    let up = WallDirection::Up;
+                    let (input, expected) = $value;
+                    let current = input.0;
+                    let walls = input.1;
 
-    let test_data = vec![(
-        new_search(0, 1, North),
-        vec![(new_wall(0, 0, right), true), (new_wall(0, 0, up), false)],
-        vec![
-            new_search(1, 2, East),
-            new_search(0, 3, North),
-            new_search(0, 1, South),
-        ],
-    )];
-
-    for (current, walls, expected) in test_data {
-        let maze = MazeBuilder::new().costs(cost).build::<U4>();
-        for (wall, exists) in walls {
-            maze.check_wall(wall, exists);
-        }
-        let candidates = solver.next_node_candidates(current, &maze);
-        assert_eq!(candidates.unwrap(), expected.as_slice());
+                    let current = new_search(current.0, current.1, current.2);
+                    let maze = MazeBuilder::new().costs(cost).build::<U4>();
+                    for (wall, exists) in walls.into_iter().map(|wall| (new_wall(wall.0,wall.1,wall.2), wall.3)) {
+                        maze.check_wall(wall, exists);
+                    }
+                    let expected = expected.into_iter().map(|input| new_search(input.0, input.1, input.2)).collect::<Vec<_>>();
+                    let candidates = solver.next_node_candidates(current, &maze);
+                    assert_eq!(candidates.unwrap(), expected.as_slice());
+                }
+        )*
     }
+}
+
+next_node_candidates_tests! {
+    test_next_node_candidates1: (
+        (
+            (0,1,North),
+            vec![
+                (0,0,Right,true),
+                (0,0,Up,false),
+            ],
+        ),
+        vec![
+            (1,2,East),
+            (0,3,North),
+            (0,1,South),
+        ],
+    ),
+    test_next_node_candidates2: (
+        (
+            (1,2,East),
+            vec![
+                (0,0,Right,true),
+                (0,0,Up,false),
+                (0,1,Right,false),
+                (0,1,Up,false),
+            ],
+        ),
+        vec![
+            (3,2,East),
+            (2,3,North),
+            (2,1,South),
+            (1,2,West),
+        ],
+    ),
 }
