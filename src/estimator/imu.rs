3edb02a2f80ba9655bf5fa28a375da2ee1b6a791
Kazuya Iimuro <ikazuya0201@gmail.com>
2020-08-30 09:40:14 +0900

    [update] Modified IMU methods.

--- src/estimator/imu.rs
+++ src/estimator/imu.rs
@@ -1,5 +1,3 @@
-use crate::utils::vector::Vector3;
-
 use quantities::{Acceleration, AngularSpeed};
 
 ///The y axis should directed to the front of the robot.
@@ -7,27 +5,6 @@ use quantities::{Acceleration, AngularSpeed};
 pub trait IMU {
     type Error;
 
-    fn get_angular_speed_x(&mut self) -> nb::Result<AngularSpeed, Self::Error>;
-    fn get_angular_speed_y(&mut self) -> nb::Result<AngularSpeed, Self::Error>;
-    fn get_angular_speed_z(&mut self) -> nb::Result<AngularSpeed, Self::Error>;
-
-    fn get_acceleration_x(&mut self) -> nb::Result<Acceleration, Self::Error>;
-    fn get_acceleration_y(&mut self) -> nb::Result<Acceleration, Self::Error>;
-    fn get_acceleration_z(&mut self) -> nb::Result<Acceleration, Self::Error>;
-
-    fn get_angular_speeds(&mut self) -> nb::Result<Vector3<AngularSpeed>, Self::Error> {
-        Ok(Vector3 {
-            x: self.get_angular_speed_x()?,
-            y: self.get_angular_speed_y()?,
-            z: self.get_angular_speed_z()?,
-        })
-    }
-
-    fn get_accelerations(&mut self) -> nb::Result<Vector3<Acceleration>, Self::Error> {
-        Ok(Vector3 {
-            x: self.get_acceleration_x()?,
-            y: self.get_acceleration_y()?,
-            z: self.get_acceleration_z()?,
-        })
-    }
+    fn get_translational_acceleration(&mut self) -> nb::Result<Acceleration, Self::Error>;
+    fn get_angular_speed(&mut self) -> nb::Result<AngularSpeed, Self::Error>;
 }
