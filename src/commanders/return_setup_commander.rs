9989fd3e4a118475e2080ad8d18fb44d7d9c78d2
Kazuya Iimuro <ikazuya0201@gmail.com>
2021-02-07 20:24:56 +0900

    [add] Implemented ReturnSetupCommander.

---
+++ src/commanders/return_setup_commander.rs
@@ -0,0 +1,65 @@
+use core::cmp::Reverse;
+
+use heapless::ArrayLength;
+use num::{Bounded, Saturating};
+use typenum::Unsigned;
+
+use super::{compute_shortest_path, BoundedNode, BoundedPathNode, Graph};
+use crate::operators::ReturnSetupCommander as IReturnSetupCommander;
+
+/// An implementation of [ReturnSetupCommander](crate::operators::ReturnSetupCommander).
+///
+/// This produces a setup command for returning to start position.
+pub struct ReturnSetupCommander<Node, Maze> {
+    current: Node,
+    start: Node,
+    maze: Maze,
+}
+
+impl<Node, Maze> ReturnSetupCommander<Node, Maze> {
+    pub fn new(current: Node, start: Node, maze: Maze) -> Self {
+        Self {
+            current,
+            start,
+            maze,
+        }
+    }
+}
+
+/// Error on [ReturnSetupCommander](ReturnSetupCommander).
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+pub struct ReturnSetupCommanderError;
+
+/// A trait that enumerates all nodes which is reachable from the given node, and also enumerates
+/// kinds of rotation.
+pub trait RotationNode: Sized {
+    type Kind;
+    type Nodes: IntoIterator<Item = (Self, Self::Kind)>;
+
+    fn rotation_nodes(&self) -> Self::Nodes;
+}
+
+impl<Node, Maze> IReturnSetupCommander for ReturnSetupCommander<Node, Maze>
+where
+    Node: BoundedPathNode + BoundedNode + Clone + Into<usize> + PartialEq + RotationNode,
+    Node::PathUpperBound: ArrayLength<Node>,
+    Node::UpperBound: Unsigned
+        + ArrayLength<Maze::Cost>
+        + ArrayLength<Option<Node>>
+        + ArrayLength<(Node, Reverse<Maze::Cost>)>
+        + ArrayLength<Option<usize>>,
+    Maze: Graph<Node>,
+    Maze::Cost: Bounded + Saturating + Copy + Ord,
+{
+    type Error = ReturnSetupCommanderError;
+    type Command = Node::Kind;
+
+    fn setup_command(&self) -> Result<Self::Command, Self::Error> {
+        for (node, kind) in self.current.rotation_nodes() {
+            if let Some(_) = compute_shortest_path(&node, &[self.start.clone()], &self.maze) {
+                return Ok(kind);
+            }
+        }
+        Err(ReturnSetupCommanderError)
+    }
+}
