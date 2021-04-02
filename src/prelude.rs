9cda904b74a99093c5a77f1ecbdffb7b3ee51060
Kazuya Iimuro <ikazuya0201@gmail.com>
2021-04-03 04:26:13 +0900

    [update] Modified prelude to display all traits as re-exports.

--- src/prelude.rs
+++ src/prelude.rs
@@ -5,39 +5,57 @@
 //! use components::prelude::*;
 //! ```
 
-pub use crate::administrator::Operator as _components_administrator_Operator;
-pub use crate::administrator::OperatorStore as _components_administrator_OperatorStore;
-pub use crate::administrator::Selector as _components_administrator_Selector;
+#[doc(no_inline)]
+pub use crate::administrator::{
+    Operator as _components_administrator_Operator,
+    OperatorStore as _components_administrator_OperatorStore,
+    Selector as _components_administrator_Selector,
+};
 
-pub use crate::operators::TrackingAgent as _components_operators_TrackingAgent;
-pub use crate::operators::TrackingCommander as _components_operators_TrackingCommander;
+#[doc(no_inline)]
+pub use crate::operators::{
+    TrackingAgent as _components_operators_TrackingAgent,
+    TrackingCommander as _components_operators_TrackingCommander,
+};
 
-pub use crate::commanders::BoundedNode as _components_commander_BoundedNode;
-pub use crate::commanders::Graph as _components_commander_Graph;
-pub use crate::commanders::NextNode as _components_commander_NextNode;
-pub use crate::commanders::NodeChecker as _components_commander_NodeChecker;
-pub use crate::commanders::RouteNode as _components_commander_RouteNode;
-pub use crate::commanders::UncheckedNodeFinder as _components_commander_GraphConverter;
+#[doc(no_inline)]
+pub use crate::commanders::{
+    BoundedNode as _components_commanders_BoundedNode, Graph as _components_commanders_Graph,
+    NextNode as _components_commanders_NextNode, NodeChecker as _components_commanders_NodeChecker,
+    RouteNode as _components_commanders_RouteNode,
+    UncheckedNodeFinder as _components_commanders_UncheckedNodeFinder,
+};
 
-pub use crate::robot::StateEstimator as _components_robot_StateEstimator;
-pub use crate::robot::Tracker as _components_robot_Tracker;
+#[doc(no_inline)]
+pub use crate::robot::{
+    StateEstimator as _components_robot_StateEstimator, Tracker as _components_robot_Tracker,
+};
 
+#[doc(no_inline)]
 pub use crate::tracker::Controller as _components_tracker_Controller;
 
-pub use crate::sensors::DistanceSensor as _components_sensors_DistanceSensor;
-pub use crate::sensors::Encoder as _components_sensors_Encoder;
-pub use crate::sensors::Motor as _components_sensors_Motor;
-pub use crate::sensors::IMU as _components_sensors_IMU;
+#[doc(no_inline)]
+pub use crate::sensors::{
+    DistanceSensor as _components_sensors_DistanceSensor, Encoder as _components_sensors_Encoder,
+    Motor as _components_sensors_Motor, IMU as _components_sensors_IMU,
+};
 
+#[doc(no_inline)]
 pub use crate::utils::math::Math as _utils_math_Math;
 
-pub use crate::mazes::GraphNode as _components_maze_GraphNode;
-pub use crate::mazes::WallChecker as _components_maze_WallManager;
-pub use crate::mazes::WallFinderNode as _components_maze_WallFinderNode;
-pub use crate::mazes::WallSpaceNode as _components_maze_WallSpaceNode;
+#[doc(no_inline)]
+pub use crate::mazes::{
+    GraphNode as _components_maze_GraphNode, WallChecker as _components_maze_WallManager,
+    WallFinderNode as _components_maze_WallFinderNode,
+    WallSpaceNode as _components_maze_WallSpaceNode,
+};
 
-pub use crate::trajectory_managers::CommandConverter as _components_trajectory_managers_CommandConverter;
-pub use crate::trajectory_managers::SearchTrajectoryGenerator as _components_trajectory_managers_SearchTrajectoryGenerator;
-pub use crate::trajectory_managers::TrackingTrajectoryGenerator as _components_trajectory_managers_TrackingTrajectoryGenerator;
+#[doc(no_inline)]
+pub use crate::trajectory_managers::{
+    CommandConverter as _components_trajectory_managers_CommandConverter,
+    SearchTrajectoryGenerator as _components_trajectory_managers_SearchTrajectoryGenerator,
+    TrackingTrajectoryGenerator as _components_trajectory_managers_TrackingTrajectoryGenerator,
+};
 
+#[doc(no_inline)]
 pub use crate::wall_detector::ObstacleDetector as _components_wall_detector_ObstacleDetector;
