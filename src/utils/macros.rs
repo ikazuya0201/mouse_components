d188a140c75407dd737c1b34b5db1e635a8ed7f8
Kazuya Iimuro <ikazuya0201@gmail.com>
2021-04-04 07:41:55 +0900

    [update] Added a macro for implementing builders.

--- src/utils/macros.rs
+++ src/utils/macros.rs
@@ -54,3 +54,55 @@ macro_rules! get_or_err {
         crate::utils::builder::ok_or($self.$field_name.take(), core::stringify!($field_name))?
     }};
 }
+
+#[doc(hidden)]
+#[macro_export]
+macro_rules! impl_with_builder {
+    (
+        $builder: ident:
+        {
+            $(#[$meta:meta])*
+            pub struct $name: ident <$($tt: tt),*> {
+                $($field_name: ident: $type: ty,)*
+            }
+        }
+    ) => {
+        $(#[$meta])*
+        pub struct $name<$($tt),*> {
+            $($field_name: $type),*
+        }
+
+        impl<$($tt),*> $name<$($tt),*> {
+            pub fn builder() -> $builder<$($tt),*> {
+                $builder::new()
+            }
+        }
+
+        pub struct $builder<$($tt),*> {
+            $($field_name: Option<$type>),*
+        }
+
+        impl<$($tt),*> $builder<$($tt),*> {
+            pub fn new() -> Self {
+                Self {
+                    $($field_name: None),*
+                }
+            }
+
+            pub fn build(&mut self) -> crate::utils::builder::BuilderResult<$name<$($tt),*>> {
+                Ok(
+                    $name {
+                        $($field_name: get_or_err!(self.$field_name),)*
+                    }
+                )
+            }
+
+            $(
+                pub fn $field_name(&mut self, $field_name: $type) -> &mut Self {
+                    self.$field_name = Some($field_name);
+                    self
+                }
+            )*
+        }
+    }
+}
