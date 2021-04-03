6b62399929f676fb4c47836ec4835acb35b029fc
Kazuya Iimuro <ikazuya0201@gmail.com>
2021-04-04 08:19:36 +0900

    [update] Implemented `DefaultPatternConverter` and its builder.

--- src/pattern_converters.rs
+++ src/pattern_converters.rs
@@ -8,6 +8,7 @@ use crate::mazes::PatternConverter;
 use crate::types::data::Pattern;
 
 impl_with_builder! {
+    /// A builder for [LinearPatternConverter].
     LinearPatternConverterBuilder:
     {
         /// An implementation of [PatternConverter](PatternConverter).
@@ -58,6 +59,51 @@ where
         match pattern {
             &Straight(x) => x.into() * self.straight_multiplier,
             &StraightDiagonal(x) => x.into() * self.straight_diagonal_multiplier,
+            Search90 => self.search90,
+            FastRun45 => self.fast_run45,
+            FastRun90 => self.fast_run90,
+            FastRun135 => self.fast_run135,
+            FastRun180 => self.fast_run180,
+            FastRunDiagonal90 => self.fast_run_diagonal90,
+            SpinBack => self.spin_back,
+        }
+    }
+}
+
+impl_with_builder! {
+    /// A builder for [DefaultPatternConverter].
+    DefaultPatternConverterBuilder:
+    {
+        /// An implementation of [PatternConverter].
+        ///
+        /// The costs of straight and straight diagonal can be determined for each length.
+        pub struct DefaultPatternConverter<Cost; const N: usize> {
+            search90: Cost,
+            fast_run45: Cost,
+            fast_run90: Cost,
+            fast_run135: Cost,
+            fast_run180: Cost,
+            fast_run_diagonal90: Cost,
+            spin_back: Cost,
+            straight_array: [Cost; N],
+            straight_diagonal_array: [Cost; N],
+        }
+    }
+}
+
+impl<Cost, const N: usize> PatternConverter<Pattern> for DefaultPatternConverter<Cost, N>
+where
+    u8: Into<Cost>,
+    Cost: core::ops::Mul<Output = Cost> + Copy,
+{
+    type Cost = Cost;
+
+    fn convert(&self, pattern: &Pattern) -> Self::Cost {
+        use Pattern::*;
+
+        match pattern {
+            &Straight(x) => self.straight_array[x as usize],
+            &StraightDiagonal(x) => self.straight_diagonal_array[x as usize],
             Search90 => self.search90,
             FastRun45 => self.fast_run45,
             FastRun90 => self.fast_run90,
